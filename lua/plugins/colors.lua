e356a7af95b5c5c582e7d8049e5c73e3aef90679
Aman Maurya <amank9thdnr@gmail.com>
2025-06-22 09:25:33 +0530

    add themes

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,20 +1,26 @@
-
 return {
-    {
-	"folke/tokyonight.nvim",
-	config = function()
-	    vim.cmd.colorscheme "tokyonight"
-	end
-    },
-    {
-      "nvim-lualine/lualine.nvim",
-      dependencies = {
-	  "nvim-tree/nvim-web-devicons",
-    }, 
+  { "folke/tokyonight.nvim" },
+  { "shaunsingh/nord.nvim" },
+  { "olimorris/onedarkpro.nvim" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "rebelot/kanagawa.nvim" },
+  { "ellisonleao/gruvbox.nvim" },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
-	theme = 'tokyonight', 
+      options = {
+        theme = "tokyonight",
+        icons_enabled = true,
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "│", right = "│" },
+      },
     },
-}
+  },
 
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
 }
-
