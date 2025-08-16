2ebbb1ddf8804277200631861cc292f34dd6ed0d
Aman <amank9thdnr@gmail.com>
2025-08-16 17:30:09 +0530

    add supermaven for code completions

--- lua/plugins/oneliners.lua
+++ lua/plugins/oneliners.lua
@@ -67,7 +67,26 @@ return {
       })
     end,
   },
- 
-  
+
+{
+  -- supermaven code completion
+  "supermaven-inc/supermaven-nvim",
+  event = "VeryLazy",
+  config = function()
+    require("supermaven-nvim").setup({
+      keymaps = {
+        accept_suggestion = "<C-l>",     -- Ctrl+l
+      },
+      ignore_filetypes = { "cpp" },
+      color = {
+        suggestion_color = "#808080",
+        cterm = 244,
+      },
+      log_level = "info",
+      disable_inline_completion = false,
+      disable_keymaps = false,
+    })
+  end,
+}
  
 }
